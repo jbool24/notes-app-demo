711491ac3217b2980f1854fc97174abf18e66542
Justin Bellero <j.bellero@dreamrocksmarttech.com>
2020-09-18 12:46:30 -0400

    [ADD]: Link to root page

--- src/layouts/default.tsx
+++ src/layouts/default.tsx
@@ -1,11 +1,15 @@
 import React, { Component } from "react";
-
+import { Link } from "react-router-dom";
 export default class DefaultLayout extends Component {
   render() {
     return (
       <div className="flex flex-col w-full h-screen">
         <header className="bg-gray-500 py-4 shadow z-50">
-          <h1 className="w-full m-auto text-4xl text-center ">📑 Notes App</h1>
+          <Link to="/">
+            <h1 className="w-full m-auto text-4xl text-center ">
+              📑 Notes App
+            </h1>
+          </Link>
         </header>
         <main className="flex-grow py-6 sm:px-6 lg:px-8 bg-gray-200 z-0">
           <section className="w-full md:w-5/6 mx-auto">
