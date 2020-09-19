3a474c07deee94685f2121f2856b29f548d50684
Justin Bellero <j.bellero@dreamrocksmarttech.com>
2020-09-18 21:37:46 -0400

    [UPDATE]: wrap emoji in aria complient tag

--- src/layouts/default.tsx
+++ src/layouts/default.tsx
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
+
 export default class DefaultLayout extends Component {
   render() {
     return (
@@ -7,13 +8,16 @@ export default class DefaultLayout extends Component {
         <header className="bg-gray-500 py-4 shadow z-50">
           <Link to="/">
             <h1 className="w-full m-auto text-4xl text-center ">
-              📑 Notes App
+              <span role="img" aria-label="note emoji">
+                📑
+              </span>
+              Notes App
             </h1>
           </Link>
         </header>
         <main className="flex-grow py-6 sm:px-6 lg:px-8 bg-gray-200 z-0">
-          <section className="w-full md:w-5/6 mx-auto">
-            <div className="px-4 py-4 sm:px-0">{this.props.children}</div>
+          <section className="w-full md:w-5/6 md:mx-auto">
+            <div className="px-0 md:px-4 md:py-4">{this.props.children}</div>
           </section>
         </main>
       </div>
