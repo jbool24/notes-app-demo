cc0cf37355905d02eb928cf3cc23cc2152630253
Justin Bellero <j.bellero@dreamrocksmarttech.com>
2020-09-18 21:38:14 -0400

    [UPDATE]: entity definitions

--- src/components/notes/index.ts
+++ src/components/notes/index.ts
@@ -1,4 +1,19 @@
-export * from "./notes.d";
-export { NoteList } from "./NoteList";
-export { NoteListItem } from "./NoteListItem";
-export { NoteCard } from "./NoteCard";
+export { default as NoteCard } from "./NoteCard";
+export { default as NoteList } from "./NoteList";
+export { default as NoteListItem } from "./NoteListItem";
+
+export type Department = {
+  id: string;
+  name: string;
+};
+
+export type Note = {
+  id: string;
+  title: string;
+  author: any;
+  note_body: string;
+  created_at: string;
+  department: Department;
+  department_role: string;
+  reviewed: boolean;
+};
