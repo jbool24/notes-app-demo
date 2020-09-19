31aac27cca88def5ff2007c7f662cc36336063d6
Justin Bellero <j.bellero@dreamrocksmarttech.com>
2020-09-18 21:31:24 -0400

    [UPDATE]: main page route to notes under "/notes" path

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
 import React from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import { ApolloProvider } from "@apollo/client";
 import Client from "./common/apollo";
 import DefaultLayout from "./layouts/default";
-import NotePage from "./pages/Notes";
+import NotePage from "./pages/NotesPage";
 
 import "./App.css";
 
@@ -20,6 +20,7 @@ function App() {
           <Route path="/" exact>
             <div className="py-2 text-center bg-white rounded-lg shadow-lg h-full">
               <h3>Default Root Page</h3>
+              <Link to="/notes">Go to Notes!</Link>
             </div>
           </Route>
         </DefaultLayout>
