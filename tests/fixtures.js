08db4dc4d67076f1d9dcfb6f9979edee295f211e
Justin Bellero <j.bellero@dreamrocksmarttech.com>
2020-09-18 21:36:28 -0400

    [UPDATE]: move test data into tests

---
+++ tests/fixtures.js
@@ -0,0 +1,32 @@
+const OFFLINE_DATA = {
+  notes: [
+    {
+      id: "1c668fac-7ec9-4602-b193-ede7b8d448cf",
+      title: "Another Note",
+      created_at: "2020-09-18T15:31:03.865604+00:00",
+      note_body: "The site was verified today",
+      reviewed: false,
+      department_role: "General Contractor",
+      department: {
+        id: "40037e36-5206-4a1e-8b57-5eaffee64c0e",
+        name: "Management"
+      }
+    },
+    {
+      id: "c9f6358e-41a1-41d3-abd4-2d04ce94bb6f",
+      title: "First Note",
+      created_at: "2020-09-18T02:33:10.438249+00:00",
+      note_body: "The site was verified today",
+      reviewed: true,
+      department_role: "Contractor",
+      department: {
+        id: "27f9a6af-ffdc-4bea-8014-ae86a93c6242",
+        name: "Engineering"
+      }
+    }
+  ]
+};
+
+module.exports = {
+  OFFLINE_DATA
+};
