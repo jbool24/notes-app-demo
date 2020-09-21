a39810de9717f7e55c70df43aa1307d738b3d2ea
Justin Bellero <j.bellero@dreamrocksmarttech.com>
2020-09-21 13:50:15 -0400

    [UPDATE]: add Auth0 user API token for Hasura request via Apollo client

--- src/common/apollo.tsx
+++ src/common/apollo.tsx
@@ -1,47 +1,64 @@
 import React from "react";
-import { useAuth0 } from "@auth0/auth0-react";
 import {
   ApolloClient,
   ApolloProvider,
   createHttpLink,
-  InMemoryCache
+  InMemoryCache,
+  GraphQLRequest
 } from "@apollo/client";
+// import { WebSocketLink } from "@apollo/client/link/ws";
 import { setContext } from "@apollo/client/link/context";
+import { useAuth0 } from "@auth0/auth0-react";
 
-export default function AuthorizedApolloProvider(props: {
-  children: JSX.Element;
-}) {
+export default function AuthdApolloProvider({ children }) {
   const { getAccessTokenSilently } = useAuth0();
-
   const httpLink = createHttpLink({
     uri: process.env.REACT_APP_GRAPHQL_ENDPOINT
   });
 
-  const authLink = setContext(async (_, { headers }) => {
-    // const token = await getAccessTokenSilently();
-    //
-    // console.log("Got Token");
-    // console.log(token);
+  // const websocketLink = new WebSocketLink({
+  //   uri: process.env.REACT_APP_GRAPHQL_WS_ENDPOINT,
+  //   options: {
+  //     reconnect: true,
+  //     connectionParams: {
+  //       headers: {
+  //         Authorization: `Bearer ${token}`
+  //       }
+  //     }
+  //   }
+  // });
+  const authLink = setContext(
+    async (_: GraphQLRequest, { headers, ...context }) => {
+      const token = await getAccessTokenSilently({
+        audience: "https://sf-hasura-notes.herokuapp.com/graphql"
+      });
 
-    let authHeaders = (token: String) => ({
-      authorization: `Bearer ${token}`,
-      "x-hasura-role": "user",
-      "x-hasura-admin-secret": process.env.REACT_APP_GRAPHQL_SECRET
-    });
-
-    return {
-      headers: {
-        ...headers,
-        // ...authHeaders(token),
-        "x-hasura-admin-secret": process.env.REACT_APP_GRAPHQL_SECRET
+      if (typeof Storage !== "undefined" && token !== undefined) {
+        localStorage.setItem("token", token);
       }
-    };
-  });
+
+      return {
+        headers: {
+          ...headers,
+          ...(token
+            ? {
+                Authorization: `Bearer ${token}`,
+                "x-hasura-role": "user"
+              }
+            : {
+                "x-hasura-admin-secret": process.env.REACT_APP_GRAPHQL_SECRET
+              })
+        },
+        ...context
+      };
+    }
+  );
 
   const client = new ApolloClient({
     link: authLink.concat(httpLink),
-    cache: new InMemoryCache()
+    cache: new InMemoryCache(),
+    connectToDevTools: process.env.NODE_ENV !== "production"
   });
 
-  return <ApolloProvider client={client}>{props.children}</ApolloProvider>;
+  return <ApolloProvider client={client}>{children}</ApolloProvider>;
 }
