0c47de3df0c098d5c16f3324dda95728ff4ec22c
Justin Bellero <j.bellero@dreamrocksmarttech.com>
2020-09-21 14:52:12 -0400

    [UDPATE]: Dropdown form component

--- src/components/notes/DepartmentsDropdown.tsx
+++ src/components/notes/DepartmentsDropdown.tsx
@@ -4,21 +4,13 @@ import { useQuery } from "@apollo/client";
 import { ALL_DEPARTMENTS_QUERY } from "../../common/queries";
 
 export default function DepartmentsDropdown({ selected, onUpdate }: any) {
-  // const [selected, setSelected] = useState({ id: 0 });
   const { loading, error, data } = useQuery(ALL_DEPARTMENTS_QUERY);
 
   const handleChange = (event: { target: HTMLSelectElement }) => {
     const department = data.departments.find(d => d.id === event.target.value);
-    // setSelected(department);
+    // Update Parent Data on change
     onUpdate(department);
-    console.log(selected);
   };
-
-  useEffect(() => {
-    if (!loading) {
-      console.log("EFFECT CALLED set defualt department on form");
-    }
-  }, [data, loading, onUpdate, selected]);
 
   if (error) {
     return (
@@ -46,7 +38,7 @@ export default function DepartmentsDropdown({ selected, onUpdate }: any) {
     <select
       value={selected.id || data.departments[0]}
       onChange={handleChange}
-      className="w-full h-full px-2 py-1 bg-white border border-gray-400"
+      className="w-full mt-1 px-2 py-1 text-sm leading-5 bg-white border border-gray-400"
     >
       {data.departments.map((d: Department) => {
         return (
